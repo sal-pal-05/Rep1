08209d4ff4c04a1ee1cb4634ebea6ca69e353cd9
sal-pal-05 <C00137488@louisiana.edu>
2019-09-18 09:47:19 -0500

    made changes

--- Rep1.R
+++ Rep1.R
@@ -13,3 +13,12 @@ fish$avg.DNE.m=as.integer(fish$avg.DNE.m)
 fish$avg.DNE.m
 class(fish$avg.DNE.m)
 fish$avg.DNE.m=as.character(fish$avg.DNE.m)
+
+fish$area_fac[[7]]
+mat=matrix (data=1:12, nrow=3, ncol=4, dimnames=list(c('r1','r2','r3'),c('c1','c2','c3','c4')))
+mat
+mat[7]=NA
+mat
+
+
+df=data.frame(a=(1, 2 ,3, 4), b=c())
